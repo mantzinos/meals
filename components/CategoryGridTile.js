c3d7f24411fd434f91ec713906007c172567322b
makis <mantzinos@hotmail.com>
2022-11-20 01:42:35 +0200

    ios shadow

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -19,7 +19,12 @@ const styles = StyleSheet.create({
     margin: 16,
     height: 150,
     borderRadius: 8,
-    elevation: 4
+    elevation: 4,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8
   },
   button: {
     flex: 1
