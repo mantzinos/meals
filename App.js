80e1fca1186966226151700db9d39d4f7d740713
makis <mantzinos@hotmail.com>
2022-11-20 01:38:28 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { CATEGORIES } from './data/dummy-data';
+import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <CategoriesScreen />;
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +12,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
