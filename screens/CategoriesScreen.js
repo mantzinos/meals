339db1089add03a0e1669dd22b0d8b6fbf55c382
makis <mantzinos@hotmail.com>
2022-11-20 02:54:47 +0200

    add navigation 2

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,13 +3,20 @@ import React from 'react';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+export default function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview');
+    }
 
-export default function CategoriesScreen() {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
   return (
     <FlatList
       data={CATEGORIES}
