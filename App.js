55de08f83a99849541cdfe950d0ef6527ccd80c5
makis <mantzinos@hotmail.com>
2022-11-20 05:35:41 +0200

    add some style 4

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { CATEGORIES } from './data/dummy-data';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -38,7 +38,15 @@ export default function App() {
             //   };
             // }}
           />
-          <Stack.Screen name='MealDetail' component={MealDetailScreen} />
+          <Stack.Screen
+            name='MealDetail'
+            component={MealDetailScreen}
+            options={{
+              headerRight: () => {
+                return <Button title='Tap me!' />;
+              }
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
